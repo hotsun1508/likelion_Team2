0ff14f890e98c8ac4c6ab00c198a61640dceaeb2
kyunghwan1207 <gkw1207@hufs.ac.kr>
2022-05-31 02:31:18 +0900

    Schedule테이블 생성 및 데이터 입력

--- team2/team2/settings.py
+++ team2/team2/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'smartscheduler',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',     # 추가
+    'django.middleware.common.CommonMiddleware', # 추가
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +123,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 이하 추가
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+#이하 추가
+CORS_ORIGIN_WHITELIST = [
+    'https://localhost:3000',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
